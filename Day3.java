4ee2c37a60602c1dbe65348ec65b594d3e04da12
Miftah Faridl. S <115477714+Miffaridell@users.noreply.github.com>
2023-10-11 12:20:34 +0800

    Add files via upload

---
+++ Day3.java
@@ -0,0 +1,12 @@
+package day3;
+
+public class Day3 {
+
+    public static void main(String[] args) {
+        int angka1 = 10;
+        int angka2 = 4;
+        int hasil = angka1 + angka2;
+        System.out.println("Hasil penjumlahan :" + hasil);
+    }
+
+}
